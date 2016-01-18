47323a3355a9dc90ad856f12879ce01a1cb0e279
phrrezende <phr.rezende@gmail.com>
2016-01-18 16:58:10 -0200

    Teste realizado com sucesso. Colocar as rotas criadas manualmente antes dos resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  get  "/convidados/busca" => "convidados#busca"
   resources :convites
   resources :convidados
   root "convidados#index"
   post "/convites/busca" => "convites#busca"
-
+ 
 end

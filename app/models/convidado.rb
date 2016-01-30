ca57e59a377b8583fcc873cc6776f9fde9c4673a
phrrezende <phr.rezende@gmail.com>
2016-01-30 18:36:46 -0200

    app/models/convidado.rb

--- app/models/convidado.rb
+++ app/models/convidado.rb
@@ -3,7 +3,7 @@ class Convidado < ActiveRecord::Base
 
 
 	def self.busca_total
-		Convidado.count()
+		Convidado.where("convidados.id not in (select convidado_id from convites)").count()
 	end
 
 	def self.sem_convite
